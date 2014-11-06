6eaee0d750745d510b1d39952bc0a805b5b60601
Jose Fresco <josemariafresco@gmail.com>
2014-11-06 13:26:33 -0300

    Added HTTPS support

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
  */
 
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('server.key', 'utf-8');
+var certificate = fs.readFileSync('server.crt', 'utf-8');
+var credentials = { key: privateKey, cert: certificate };
+
 var app = module.exports = require('lib/boot');
 var server = http.createServer(app);
+var secureServer = https.createServer(credentials, app);
+
 var balance = require('lib/balance');
 var config = require('lib/config');
 var log = require('debug')('democracyos:root');
@@ -17,6 +25,9 @@ if (module === require.main) {
   balance(function() {
     server.listen(config('privatePort'), function() {
       log('Application started on port %d', config('privatePort'));
+    });
+    secureServer.listen(443, function() {
+      log('Secure port enabled');
     });
   });
 }
